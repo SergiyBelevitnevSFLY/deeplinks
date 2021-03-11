ac9d683b559cc539b9a198213075c00b55a3b1d2
sergiy-belevitnev <sergiy.belevitnev@shutterfly.com>
2021-03-11 15:01:23 +0200

    added skus

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -249,6 +249,109 @@ import { Component, OnInit } from '@angular/core';
   <button type="button" class="btn btn-secondary"> 
   <a href="shutterfly-staging://mobile/v2/products?products=[1499788]">1499788</a> 
   </button>
+
+
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1299741]">1299741</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1080612]">1080612</a> 
+  </button>
+  
+
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1349317]">1349317</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1405946]">1405946</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1160808]">1160808</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1406152]">1406152</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1174054]">1174054</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1278413]">1278413</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1258184]">1258184</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1206487]">1206487</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1231678]">1231678</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1524943]">1524943</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1086052]">1086052</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1512310]">1512310</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1512650]">1512650</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1427139]">1427139</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1351527]">1351527</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[1499788]">1499788</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[PB_6X6_02]">PB_6X6_02</a> 
+  </button>
+  
+    
+  <button type="button" class="btn btn-secondary"> 
+  <a href="shutterfly-staging://mobile/v2/products?products=[WALLCAL_8X11]">WALLCAL_8X11</a> 
+  </button>
+  
   
 
 
